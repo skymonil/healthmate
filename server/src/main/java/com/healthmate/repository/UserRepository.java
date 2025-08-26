5ed23202b24a0b183aafbdb97417a46250172547
adityapai05 <adityapai.main@gmail.com>
2025-08-26 20:18:48 +0530

    Implement user cleanup service to remove expired unverified users every 5 minutes

--- server/src/main/java/com/healthmate/repository/UserRepository.java
+++ server/src/main/java/com/healthmate/repository/UserRepository.java
@@ -2,8 +2,12 @@ package com.healthmate.repository;
 
 import com.healthmate.model.User;
 import org.springframework.data.mongodb.repository.MongoRepository;
+
+import java.time.LocalDateTime;
+import java.util.List;
 import java.util.Optional;
 
 public interface UserRepository extends MongoRepository<User, String> {
     Optional<User> findByEmail(String email);
+    List<User> findByVerifiedFalseAndOtpGeneratedAtBefore(LocalDateTime cutoff);
 }
